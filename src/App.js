50034aad42263a745add23b3a34ae3529e1e210b
Dipankar chowdhury <dipankarchoudhury.28.dc@gmail.com>
2019-08-16 15:49:51 +0530

    reactjs project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Nav from './Nav'
+import About from './About'
+import Shop from './Shop'
+import Home from './Home'
+import InputDetails from './InputDetails'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
 
-export default App;
+    render() {
+
+      return (
+        <Router>
+        <div className="container">
+          <Nav />
+          <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About}/>
+          <Route path="/shop" component={Shop}/>
+          <Route path="/inputdetials" component={InputDetails}/>
+          </Switch>
+        </div>
+        </Router>
+      )
+    }
+
+  }
+
+export default App
